08f3e563dea54a8ef1156dd2cd188b914ea2e2ce
Yuliya Sidarenka <yulka1102@gmail.com>
2022-09-30 14:18:53 +0200

    initial html with little styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [users, setUsers] = useState([]);
+    const [name, setName] = useState('');
+    const [age, setAge] = useState(0);
+
+    const handleClick = (e) => {
+      e.preventDefault();
+    }
+
+    return (
+        <div className='container'>
+            <form className="form">
+                <input className="form-input" value={name} onChange={(e) => setName(e.target.value)} type="text"/>
+                <input className="form-input" value={age} onChange={(e) => setAge(parseInt(e.target.value))} type="number"/>
+
+                <div className="buttons">
+                    <button onClick={handleClick}>Create</button>
+                    <button>Get</button>
+                </div>
+            </form>
+            <ul className="list">
+                {users.map(({ id, username, age }) =>
+                    <li key={id}>
+                        <div>
+                            <h3>Name: {username}</h3>
+                            <h6>Age: {age}</h6>
+                        </div>
+                    </li>
+                )}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
