1645c54c12386a5e916a0c85094de87392542d0b
Yuliya Sidarenka <yulka1102@gmail.com>
2022-09-30 11:36:53 +0200

    init graphql graphic

--- server/index.js
+++ server/index.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const cors = require('cors');
+
 
 const app = express();
+app.use(cors());
+
+app.use('/graphql', graphqlHTTP({
+    graphiql: true
+}))
 
 app.listen(5000, () => console.log('server started on port 5000'));
